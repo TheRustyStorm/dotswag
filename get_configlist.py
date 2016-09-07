dd8d398a19150bfe8c355391ecb06ed6e9e94d06
TheSovietStorm <TheSovietStorm@users.noreply.github.com>
2016-09-07 14:33:32 +0200

    Update get_configlist.py

--- get_configlist.py
+++ get_configlist.py
@@ -1,7 +1,6 @@
 import requests
 import os
-r=requests.get("https://raw.githubusercontent.com/TheSovietStorm/dotswag/master/.swagconf")
-print(r.text)
+r=requests.get("https://raw.githubusercontent.com/TheSovietStorm/dotswag/master/.swagconf")  #Edit this Link to your swagconf
 swagconf=open(os.getenv("HOME")+"/.swagconf",'w')
 for i in r.text:
     swagconf.write(i)
