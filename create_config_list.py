d7efd188b9e38709c5aa3a5d98e2646c67651707
Peter <pzde95@yahoo.de>
2016-09-07 12:26:19 +0200

    first script works

--- create_config_list.py
+++ create_config_list.py
@@ -1,1 +1,10 @@
-print("Test")
+import os
+inputfile=open(os.getenv("HOME")+'/.swagconf','r')          #This is the masterconfig, all headers are read here
+outputfile=open(os.getenv("HOME")+'/.swagsources','w')      #To choose, which configs have to be read, in this file every used tag is saved
+for line in inputfile:                                      #We check every line, whether it starts with our beatiful tag
+    if line[0:10] == '###DOTSWAG':
+        splitted_line=line.split('"')                       #We split on " , because a path can contain wanky symbols or spaces
+        outputfile.write(splitted_line[1]+'\n')             #Every tag is written into a new line in the outputfile
+inputfile.close()                                           #Close each file
+outputfile.close()
+
